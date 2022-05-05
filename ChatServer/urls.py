120dfc6041d7fe2000f2eb38201e3e68cda6fcc9
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-05-05 20:52:58 +0500

    Static added

--- ChatServer/urls.py
+++ ChatServer/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from ChatServer.settings import STATIC_ROOT
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('personal/', include("personal.urls"))
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
